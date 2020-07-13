67b3e46fb399f2bcaa648b6e416c1e982b7babc7
Marco Argentieri <3596602+tiero@users.noreply.github.com>
2020-07-13 10:07:07 +0200

    Drop dist folder

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { networks } from 'liquidjs-lib';
-export * from './swap';
-export * from './trade';
-export * from './grpcClient';
-export * from './wallet';
-export { calculateExpectedAmount, calculateProposeAmount } from './utils';
