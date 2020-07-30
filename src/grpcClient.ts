a0a6bbb7c46a9576b8282295a61e18537da0652c
Marco Argentieri <3596602+tiero@users.noreply.github.com>
2020-07-30 18:55:12 +0200

    isBrowser function

--- src/grpcClient.ts
+++ src/grpcClient.ts
@@ -1,9 +1,12 @@
 import * as web from './grpcNodeClient';
 import * as node from './grpcNodeClient';
 
-// eslint-disable-next-line
-const isBrowser = new Function(
-  'try { return this === window; } catch (e) { return false; } }'
-);
+const isBrowser = () => {
+  try {
+    return window !== undefined;
+  } catch (e) {
+    return false;
+  }
+}
 
 export const TraderClient = isBrowser() ? web.TraderClient : node.TraderClient;
