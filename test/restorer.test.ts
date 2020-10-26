a5c5057c5ce89a507c7df26ee5d0841f3be27218
Louis Singer <louis.singer.git@gmail.com>
2020-10-26 15:04:22 +0100

    add test case false to restorer.test.ts

--- test/restorer.test.ts
+++ test/restorer.test.ts
@@ -5,16 +5,25 @@ import * as assert from 'assert';
 
 jest.setTimeout(15000);
 
-const address =
+const addressToFaucet =
   'AzpqS7wVhDLhohSr3xmEWthdYBm9vtrzQeZ3TmjTLoCSCyJn6TjA1VMuDSLwdFeCRY7LVxPvwgDNJmGc';
+
+const addressToNotFaucet =
+  'AzpjnpWdDRSFnWC4Ymzwn3b8srTf7aLVRt5Ci13K5KpsUFkGTNQA6qHwxRNGDWH5gztp7wu3cEnZciY2';
 
 describe('EsploraIdentityRestorer', () => {
   describe('EsploraIdentityRestorer.addressHasBeenUsed', () => {
     it('should return true if the address has txs', async () => {
-      await faucet(address);
+      await faucet(addressToFaucet);
       const restorer = new EsploraIdentityRestorer();
-      const result = await restorer.addressHasBeenUsed(address);
+      const result = await restorer.addressHasBeenUsed(addressToFaucet);
       assert.deepStrictEqual(result, true);
+    });
+
+    it('should return false if the address has no txs', async () => {
+      const restorer = new EsploraIdentityRestorer();
+      const result = await restorer.addressHasBeenUsed(addressToNotFaucet);
+      assert.deepStrictEqual(result, false);
     });
   });
 });
