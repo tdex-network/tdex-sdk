f1a04525a0beae76b375f570f9c0888f8d680f57
Louis Singer <41042567+louisinger@users.noreply.github.com>
2020-10-27 15:57:22 +0100

    Check if SwapFail in TradeProposeReply & TradeCompleteReply (#47)
    * check if SwapFail exists in TradeProposeReply and TradeCompleteReply
    
    * throw error if SwapFail in Trade*Reply instead of reject promises
    
    * throwErrorIfSwapFail unit testing

--- test/trade.test.ts
+++ test/trade.test.ts
@@ -1,5 +1,11 @@
 import * as TDEX from '../src/index';
-import { Trade, IdentityType } from '../src/index';
+import { Trade, IdentityType, throwErrorIfSwapFail } from '../src/index';
+import {
+  TradeCompleteReply,
+  TradeProposeReply,
+} from 'tdex-protobuf/generated/js/trade_pb';
+import { SwapFail } from 'tdex-protobuf/generated/js/swap_pb';
+import * as assert from 'assert';
 
 const signingKeyWIF = 'cQ1KJtXR2WB9Mpn6AEmeUK4yWeXAzwVX7UNJgQCF9anj3SrxjryV';
 const blindingKeyWIF = 'cQ1KJtXR2WB9Mpn6AEmeUK4yWeXAzwVX7UNJgQCF9anj3SrxjryV';
@@ -27,6 +33,47 @@ describe('TDEX SDK', () => {
       verbose: false,
       providerUrl: 'localhost:9945',
       explorerUrl: 'https://nigiri.network',
+    });
+  });
+
+  describe('TraderClient', () => {
+    const swapFail = new SwapFail();
+    swapFail.setId('00011101');
+    swapFail.setFailureCode(666);
+    swapFail.setFailureMessage('COVID');
+
+    describe('TradePropose', () => {
+      const tradeProposeReply = new TradeProposeReply();
+      tradeProposeReply.setSwapFail(swapFail);
+
+      const tradeProposeReplyWithoutSwapFail = new TradeProposeReply();
+
+      it('should throw an error if there is SwapFail in TradeProposeReply', () => {
+        assert.throws(() => throwErrorIfSwapFail(tradeProposeReply));
+      });
+
+      it('should not throw an error if there is no SwapFail in TradeProposeReply', () => {
+        assert.doesNotThrow(() =>
+          throwErrorIfSwapFail(tradeProposeReplyWithoutSwapFail)
+        );
+      });
+    });
+
+    describe('TradeComplete', () => {
+      const tradeCompleteReply = new TradeCompleteReply();
+      tradeCompleteReply.setSwapFail(swapFail);
+
+      const tradeCompleteReplyWithoutSwapFail = new TradeCompleteReply();
+
+      it('should throw an error if there is SwapFail in TradeCompleteReply', () => {
+        assert.throws(() => throwErrorIfSwapFail(tradeCompleteReply));
+      });
+
+      it('should not throw an error if there is no SwapFail in TradeCompleteReply', () => {
+        assert.doesNotThrow(() =>
+          throwErrorIfSwapFail(tradeCompleteReplyWithoutSwapFail)
+        );
+      });
     });
   });
 });
