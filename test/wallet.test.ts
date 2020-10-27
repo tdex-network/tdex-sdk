abe27aebae1efd23a159bfef4e6c436ed05d2ae7
Louis Singer <louis.singer.git@gmail.com>
2020-10-27 09:13:42 +0100

    add test for SwapAccept message in wallet.test.ts

--- test/wallet.test.ts
+++ test/wallet.test.ts
@@ -1,7 +1,7 @@
 import { UtxoInterface, Wallet, walletFromAddresses } from './../src/wallet';
-import { networks, TxOutput, Transaction } from 'liquidjs-lib';
+import { networks, TxOutput, Transaction, Psbt } from 'liquidjs-lib';
 import { faucet, fetchUtxos, fetchTxHex, mint } from './_regtest';
-// import * as assert from 'assert';
+import * as assert from 'assert';
 import { Swap } from '../src/swap';
 import PrivateKey from '../src/identities/privatekey';
 import { IdentityType } from '../src/identity';
@@ -33,9 +33,14 @@ const responderAddress = responder.getNextAddress().confidentialAddress;
 jest.setTimeout(15000);
 
 describe('Wallet - Transaction builder', () => {
+  let messageSwapRequest: Uint8Array;
+  let txSwapRequest: Psbt;
+  let shitcoin: string;
+  let inputsKeys: Record<string, Buffer>;
+  let outputsKeys: Record<string, Buffer>;
+
   describe('SwapRequest transaction', () => {
     let proposerUtxos: UtxoInterface[];
-    let shitcoin: string;
 
     it('should let the Proposer to create a valid transaction to be used in a SwapRequest message', async () => {
       // found the proposer account with LBTC
@@ -79,18 +84,78 @@ describe('Wallet - Transaction builder', () => {
         proposer.getNextAddress()
       );
 
+      inputsKeys = { ...inputBlindingKeys };
+      outputsKeys = { ...outputBlindingKeys };
+
+      assert.doesNotThrow(() => (txSwapRequest = Psbt.fromBase64(psetBase64)));
+
       const swap = new Swap();
-      // assert.doesNotThrow(() =>
-      swap.request({
-        amountToBeSent: 1_0000_0000,
-        assetToBeSent: network.assetHash,
-        amountToReceive: 100_0000_0000,
-        assetToReceive: shitcoin,
-        psbtBase64: psetBase64,
+      assert.doesNotThrow(() => {
+        messageSwapRequest = swap.request({
+          amountToBeSent: 1_0000_0000,
+          assetToBeSent: network.assetHash,
+          amountToReceive: 100_0000_0000,
+          assetToReceive: shitcoin,
+          psbtBase64: psetBase64,
+          inputBlindingKeys,
+          outputBlindingKeys,
+        });
+      });
+    });
+  });
+
+  describe('SwapAccept message', () => {
+    it('should let the proposer to create a valid transaction to be used for a Swap Accept', async () => {
+      const responderUtxos = await fetchUtxos(responderAddress);
+
+      const txHexs: string[] = await Promise.all(
+        responderUtxos.map((utxo: any) => fetchTxHex(utxo.txid))
+      );
+
+      const outputs: TxOutput[] = txHexs.map(
+        (hex, index) =>
+          Transaction.fromHex(hex).outs[responderUtxos[index].vout]
+      );
+
+      responderUtxos.forEach((utxo: any, index: number) => {
+        utxo.prevout = outputs[index];
+      });
+
+      // create the request tx using wallet
+      const responderWallet: Wallet = walletFromAddresses(
+        responder.getAddresses(),
+        'regtest'
+      );
+
+      const {
+        psetBase64,
         inputBlindingKeys,
         outputBlindingKeys,
+      } = responderWallet.updateTx(
+        txSwapRequest.toBase64(),
+        responderUtxos,
+        100_0000_0000,
+        1_0000_0000,
+        shitcoin,
+        network.assetHash,
+        responder.getNextAddress(),
+        responder.getNextChangeAddress()
+      );
+
+      assert.doesNotThrow(() => Psbt.fromBase64(psetBase64));
+
+      inputsKeys = { ...inputsKeys, ...inputBlindingKeys };
+      outputsKeys = { ...outputsKeys, ...outputBlindingKeys };
+
+      const swap = new Swap();
+      assert.doesNotThrow(() => {
+        swap.accept({
+          message: messageSwapRequest,
+          psbtBase64: psetBase64,
+          inputBlindingKeys: inputsKeys,
+          outputBlindingKeys: outputsKeys,
+        });
       });
-      // );
     });
   });
 });
