49c684af574cf50c2b59efcfe854d17935219be7
Marco Argentieri <3596602+tiero@users.noreply.github.com>
2021-01-14 13:46:41 +0100

    Export utils and MasterPublicKey identity (#69)
    * Export utils and MasterPublicKey identity
    
    * lint

--- src/index.web.ts
+++ src/index.web.ts
@@ -5,6 +5,13 @@ export * from './grpcClient.web';
 export * from './identity';
 export * from './identities/mnemonic';
 export * from './identities/privatekey';
+export * from './identities/masterpubkey';
 export * from './wallet';
 export * from './types';
 export * from './identityRestorer';
+export {
+  toXpub,
+  fromXpub,
+  isValidXpub,
+  isValidExtendedBlindKey,
+} from './utils';
