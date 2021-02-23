ec25b8fbb3f3667c4bb173d2ae7de2075b193031
Marco Argentieri <3596602+tiero@users.noreply.github.com>
2021-02-23 01:35:58 +0100

    clean up

--- test/swap.test.ts
+++ test/swap.test.ts
@@ -27,9 +27,9 @@ describe('Swap', () => {
     'cHNldP8BAP1lAQIAAAAAAu1FUTUGQ6cvcZgqyRmduZP/jCOJf6CiVj9NIvClxau5AQAAAAD/////5gvIxOksvm3xwVCvGRe1AQLH8z0utX4L0e30r+VPt5cAAAAAAP////8EASWyUQcOKcoZBDzzPM1zJOLdqwPsxK4LXnfE/A5c9slaAQAAAAAATEtAABYAFMUoyu4AUgBlfAS1xfajCz0+UeV5AdMRFPznA5TB+dNUdQG0udNvQgI27GQZkVRWZDSIWs8tAQAADZ15iEQAABYAFMUoyu4AUgBlfAS1xfajCz0+UeV5AdMRFPznA5TB+dNUdQG0udNvQgI27GQZkVRWZDSIWs8tAQAAAAb8I6wAABYAFJJ8X9Wg477+b6SuqlazoT+3x+BHASWyUQcOKcoZBDzzPM1zJOLdqwPsxK4LXnfE/A5c9slaAQAAAAAFqZXAABYAFJJ8X9Wg477+b6SuqlazoT+3x+BHAAAAAAABAUIB0xEU/OcDlMH501R1AbS5029CAjbsZBmRVFZkNIhazy0BAAANpHWr8AAAFgAUxSjK7gBSAGV8BLXF9qMLPT5R5XkiAgJp6A6eYQgEnPKMfCH5c49w+9u63C62sGGTzHIJL4ZaxEcwRAIgdN5MddCGTC9hRWvUbIOREbVwhEcARauaHT4pqavp9yACIBpEdlr8hBM6e6+S6cNqDkqkqVV0JTYqKuMt5FW/abJyAQABAUIBJbJRBw4pyhkEPPM8zXMk4t2rA+zErgted8T8Dlz2yVoBAAAAAAX14QAAFgAUknxf1aDjvv5vpK6qVrOhP7fH4EciAgKARV6Ey/QsHwC8l9zhIs/VVfUXSnp5scFgq3OrfFuzyUcwRAIgekjOvqns4gaFbY/M6Lqb8cobkai2ea3lOnKUy45KsTgCICHF1CqUIaTiztBomcDZ1VBziV9YE5XcUEdrDbmwzzERAQAAAAAA';
 
   describe('Swap Request message', () => {
-    test('should create a valid SwapRequest message if transaction is unconfidential.', () => {
+    test('should create a valid SwapRequest message if transaction is unconfidential.', async () => {
       const psetBase64 = initialPsbtOfAlice;
-      const bytes = swap.request({
+      const bytes = await swap.request({
         assetToBeSent: fixtures.assets.USDT,
         amountToBeSent: 30000000000,
         assetToReceive: fixtures.assets.LBTC,
@@ -51,7 +51,10 @@ describe('Swap', () => {
     });
 
     const psetBase64 = initialPsbtOfBob;
-    const bytes = swap.accept({ message: swapRequestMessage, psetBase64 });
+    const bytes = await swap.accept({
+      message: swapRequestMessage,
+      psetBase64,
+    });
 
     expect(bytes).toBeDefined();
   });
@@ -128,20 +131,19 @@ describe('Swap', () => {
         .toBase64();
     });
 
-    function swapRequest() {
-      return swap.request({
-        amountToBeSent: 1_0000_0000,
-        assetToBeSent: networks.regtest.assetHash,
-        amountToReceive: 100_0000_0000,
-        assetToReceive: altcoin,
-        psetBase64: requestTx,
-        inputBlindingKeys,
-        outputBlindingKeys: {},
-      });
-    }
-
     test('should create a valid swap request message', () => {
-      assert.doesNotThrow(() => swapRequest());
+      assert.doesNotThrow(
+        async () =>
+          await swap.request({
+            amountToBeSent: 1_0000_0000,
+            assetToBeSent: networks.regtest.assetHash,
+            amountToReceive: 100_0000_0000,
+            assetToReceive: altcoin,
+            psetBase64: requestTx,
+            inputBlindingKeys,
+            outputBlindingKeys: {},
+          })
+      );
     });
   });
 
