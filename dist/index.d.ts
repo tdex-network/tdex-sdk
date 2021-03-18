1d8e4ef2c2e545bd8c01c32a55895eb4bbce0baa
louisinger <louis@vulpem.com>
2021-03-18 12:07:43 +0100

    ignore dist folders

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from 'ldk';
-export * from './swap';
-export * from './trade';
-export * from './trade-core';
-export * from './grpcClient';
-export * from './transaction';
--- dist/trade.d.ts
+++
@@ -1,4 +0,0 @@
-import { TradeCore, TradeInterface, TradeOpts } from './trade-core';
-export declare class Trade extends TradeCore implements TradeInterface {
-    constructor(args: TradeOpts);
-}
