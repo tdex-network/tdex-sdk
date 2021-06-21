994f36fb980fcf165c28c2c477cadcaad82b5eaf
Marco Argentieri <3596602+tiero@users.noreply.github.com>
2021-06-21 11:17:47 +0200

    working on integration test

--- test/trade.integration.test.ts
+++ test/trade.integration.test.ts
@@ -1,89 +1,51 @@
-import { IdentityOpts, PrivateKey, PrivateKeyOpts } from 'ldk';
 import {
-  Trade,
-  IdentityType,
-  TradeType,
-  greedyCoinSelector,
-} from '../src/index';
+  IdentityOpts,
+  MnemonicOpts,
+  fetchAndUnblindUtxos,
+  Mnemonic,
+} from 'ldk';
+import { Trade, IdentityType, greedyCoinSelector } from '../src/index';
 import { sleep } from './_regtest';
-
-const signingKeyWIF = 'cQ1KJtXR2WB9Mpn6AEmeUK4yWeXAzwVX7UNJgQCF9anj3SrxjryV';
-const blindingKeyWIF = 'cQ1KJtXR2WB9Mpn6AEmeUK4yWeXAzwVX7UNJgQCF9anj3SrxjryV';
 
 const market = {
   baseAsset: '5ac9f65c0efcc4775e0baec4ec03abdde22473cd3cf33c0419ca290e0751b225',
   quoteAsset:
-    '6d0910822769196b2a3bd3eaad9ca10d43b8adf0b851607460729ec2b0b8fed0',
+    '289ac498a544d00fa2a321faa8e4f7d16ae4a62158e3f6584d5fb32b89dfd3e1',
 };
 
-const identityOpts: IdentityOpts<PrivateKeyOpts> = {
+const identityOpts: IdentityOpts<MnemonicOpts> = {
   chain: 'regtest',
-  type: IdentityType.PrivateKey,
+  type: IdentityType.Mnemonic,
   opts: {
-    signingKeyWIF,
-    blindingKeyWIF,
+    mnemonic:
+      'pitch sun weapon improve strong mail faith skull quote kitten smooth sail',
   },
 };
 
-const identity = new PrivateKey(identityOpts);
+const explorerUrl = 'http://localhost:3001';
+
+const identity = new Mnemonic(identityOpts);
+
 describe('Integration tests with a local daemon', () => {
-  test.skip('Should get the preview of a trade of a daemon with AMM', async () => {
-    const trade = new Trade({
-      providerUrl: 'localhost:9945',
-      explorerUrl: 'localhost:3001',
-      utxos: [],
-      coinSelector: greedyCoinSelector(),
-    });
-
-    const preview = await trade.preview({
-      market,
-      tradeType: TradeType.SELL,
-      amount: 100000,
-      asset: market.baseAsset,
-    });
-
-    const previewQuote = await trade.preview({
-      market,
-      tradeType: TradeType.SELL,
-      amount: 44250000,
-      asset: market.quoteAsset,
-    });
-
-    const rePreview = await trade.preview({
-      market,
-      tradeType: TradeType.SELL,
-      amount: 93651,
-      asset: market.baseAsset,
-    });
-
-    console.log(preview);
-    console.log(previewQuote);
-    console.log(rePreview);
-
-    /*   expect(preview).toStrictEqual({
-        assetToBeSent: market.baseAsset,
-        amountToBeSent: 5000000,
-        assetToReceive: market.quoteAsset,
-        amountToReceive: 23869047,
-      }); */
-  });
-
-  test.skip('Should sell some LBTCs with a daemon', async () => {
+  test('Should sell some LBTCs with a daemon', async () => {
+    const address = await identity.getNextAddress();
+    console.log(address);
     // address
-    //el1qqfv793wyh4wcz4eys9y9vu97hfdskgjedykn4jcv37qhgtjlm8xhdlfjj8mh8lrplllcwvka0tu5yyywaptwztawfdeqzdwys
+    //el1qq2ut5fy27yyc9zgftd4kagkqp2d9wne8p5l8ljad65n6a0ggschlsya0yhrxg0qwsnuysj74s8dcztdrhacw2sys9z5l8wpg4
+    const utxos = await fetchAndUnblindUtxos([address], explorerUrl);
     // blidning
     // 48566cd9b86dfd4107d615bc4b929fc63347d72238a16844e657c60fe4593ffc
     const trade = new Trade({
       providerUrl: 'localhost:9945',
-      explorerUrl: 'localhost:3001',
-      utxos: [],
+      explorerUrl,
+      utxos,
       coinSelector: greedyCoinSelector(),
     });
 
     try {
       const txid = await trade.sell({
         market,
-        amount: 100000,
+        amount: 5000,
         asset: market.baseAsset,
         identity,
       });
