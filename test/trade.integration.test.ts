c53e833bb721239cbfa53b279092525df2e3b52c
Marco Argentieri <3596602+tiero@users.noreply.github.com>
2021-06-22 10:28:16 +0200

    update integration test regtest

--- test/trade.integration.test.ts
+++ test/trade.integration.test.ts
@@ -1,16 +1,16 @@
 import {
   IdentityOpts,
-  MnemonicOpts,
   fetchAndUnblindUtxos,
   Mnemonic,
+  MnemonicOpts,
 } from 'ldk';
 import { Trade, IdentityType, greedyCoinSelector } from '../src/index';
-import { sleep } from './_regtest';
+//import { sleep } from './_regtest';
 
 const market = {
   baseAsset: '5ac9f65c0efcc4775e0baec4ec03abdde22473cd3cf33c0419ca290e0751b225',
   quoteAsset:
-    '289ac498a544d00fa2a321faa8e4f7d16ae4a62158e3f6584d5fb32b89dfd3e1',
+    'e2853f3266edc8d1d46d7c3e682cc7d9735c897a6acb2382b6f7277cf06d0904',
 };
 
 const identityOpts: IdentityOpts<MnemonicOpts> = {
@@ -18,7 +18,7 @@ const identityOpts: IdentityOpts<MnemonicOpts> = {
   type: IdentityType.Mnemonic,
   opts: {
     mnemonic:
-      'pitch sun weapon improve strong mail faith skull quote kitten smooth sail',
+      'outer prosper fish exclude pitch jaguar hole water head cream glimpse drive',
   },
 };
 
@@ -28,11 +28,16 @@ const identity = new Mnemonic(identityOpts);
 
 describe('Integration tests with a local daemon', () => {
   test('Should sell some LBTCs with a daemon', async () => {
-    const address = await identity.getNextAddress();
-    console.log(address);
+    /*     const restoredMnemonic = await mnemonicRestorerFromEsplora(identity)({
+          gapLimit: 30,
+          esploraURL: explorerUrl,
+        }); */
+
     // address
-    //el1qq2ut5fy27yyc9zgftd4kagkqp2d9wne8p5l8ljad65n6a0ggschlsya0yhrxg0qwsnuysj74s8dcztdrhacw2sys9z5l8wpg4
-    const utxos = await fetchAndUnblindUtxos([address], explorerUrl);
+    //el1qqdjz2azu3wkwvsxpy499er7ar6rxwdwcn8cc2zcl3achutwuhv65rd6spsrf2lnsrddyrlhxahj0cluzczam2pt960mkdpa9u
+    await identity.getNextAddress();
+    const addresses = await identity.getAddresses();
+    const utxos = await fetchAndUnblindUtxos(addresses, explorerUrl);
     // blidning
     // 48566cd9b86dfd4107d615bc4b929fc63347d72238a16844e657c60fe4593ffc
     const trade = new Trade({
@@ -55,15 +60,15 @@ describe('Integration tests with a local daemon', () => {
       console.error(e);
     }
 
-    await sleep(1500);
-
-    const txid3 = await trade.buy({
-      market,
-      amount: 10000,
-      asset: market.baseAsset,
-      identity,
-    });
-    console.log(txid3);
-    expect(txid3).toBeDefined();
-  }, 60000);
+    /*     await sleep(1500);
+    
+        const txid3 = await trade.buy({
+          market,
+          amount: 10000,
+          asset: market.baseAsset,
+          identity,
+        });
+        console.log(txid3);
+        expect(txid3).toBeDefined(); */
+  }, 360000);
 });
