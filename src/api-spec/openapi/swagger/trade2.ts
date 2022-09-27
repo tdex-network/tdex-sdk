db7c7c9307735d888b9c0384d319c45f18b205c7
Janaka-Steph <rochestephane@yahoo.fr>
2022-09-27 12:48:25 +0200

    use axios, add test

--- src/api-spec/openapi/swagger/trade2.ts
+++ src/api-spec/openapi/swagger/trade2.ts
@@ -1,6 +1,7 @@
 import TraderClientInterface from "grpcClientInterface";
-import {BalanceWithFee, Market, Preview, TradeType} from "api-spec/protobuf/gen/js/tdex/v1/types_pb";
-import {SwapRequest} from "api-spec/protobuf/gen/js/tdex/v1/swap_pb";
+import {BalanceWithFee, Market, Preview, TradeType} from "../../protobuf/gen/js/tdex/v1/types_pb";
+import {SwapRequest} from "../../protobuf/gen/js/tdex/v1/swap_pb";
+import axios from "axios";
 
 export class TradeServiceHttp implements TraderClientInterface {
   client: any;
@@ -12,28 +13,24 @@ export class TradeServiceHttp implements TraderClientInterface {
 
   async completeTrade(swapCompleteSerialized: Uint8Array): Promise<string> {
     const path = `/v1/trade/complete`;
-    const response = await fetch(this.providerUrl + path, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(swapCompleteSerialized),
+    const response = await axios.post(this.providerUrl + path, {
+     swapCompleteSerialized,
     })
-    const str = await response.text()
+    const str = await response.data
     console.log('completeTrade', str);
     return str
   }
 
   async proposeTrade(market: Market, tradeType: TradeType, swapRequestSerialized: Uint8Array): Promise<Uint8Array> {
     const path = `/v1/trade/propose`;
-    const response = await fetch(this.providerUrl + path, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
+    console.log('this.providerUrl', this.providerUrl);
+    const response = await axios.post(this.providerUrl + path, {
         market: market,
         type: tradeType.toString(),
-        swapRequest: SwapRequest.fromBinary(swapRequestSerialized),
-      }),
-    })
-    const str = await response.json()
+        swapRequest: SwapRequest.toJsonString(SwapRequest.fromBinary(swapRequestSerialized)),
+        //swapRequest: Buffer.from(swapRequestSerialized).toString('base64'),
+      })
+    const str = await response.data
     console.log('proposeTrade', str);
     return str
   }
@@ -42,35 +39,29 @@ export class TradeServiceHttp implements TraderClientInterface {
 
   async balance(market: Market): Promise<BalanceWithFee> {
     const path = `/v1/trade/balance`;
-    const response = await fetch(this.providerUrl + path, {
-      method: 'POST', body: JSON.stringify(market),
-    })
-    const balanceWithFee = await response.json()
+    const response = await axios.post(this.providerUrl + path, {market})
+    const balanceWithFee = await response.data
     console.log('balance', balanceWithFee);
     return balanceWithFee
   }
 
   async marketPrice(market: Market, tradeType: TradeType, amount: number, asset: string): Promise<Preview[]> {
     const path = `/v1/trade/preview`;
-    const response = await fetch(this.providerUrl + path, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
+    const response = await axios.post(this.providerUrl + path, {
         market,
         type: tradeType.toString(),
         amount,
         asset
-      }),
     })
-    const previewArray = await response.json()
+    const previewArray = await response.data
     console.log('marketPrice', previewArray);
     return previewArray
   }
 
   async markets(): Promise<Array<{ baseAsset: string; quoteAsset: string; feeBasisPoint: number }>> {
     const path = `/v1/markets`;
-    const response = await fetch(this.providerUrl + path)
-    const markets = await response.json()
+    const response = await axios.get(this.providerUrl + path)
+    const markets = await response.data
     console.log('markets', markets);
     return markets
   }
---
+++ test/tradeHttp.test.ts
@@ -0,0 +1,44 @@
+import { Psbt } from 'liquidjs-lib';
+import { TradeServiceHttp } from '../src/api-spec/openapi/swagger/trade2';
+import { Swap } from '../src';
+import { TradeType } from '../src/api-spec/protobuf/gen/js/tdex/v1/types_pb';
+import * as fixtures from './fixtures/swap.json';
+
+describe('TradeServiceHttp', () => {
+  it('propose trade', async () => {
+    const fixture = fixtures.confidentialSwaps[0];
+    const decodedAcceptPsbt = Psbt.fromBase64(fixture.accept.psbt);
+    const trade = new TradeServiceHttp('https://localhost:9945');
+    const swap = new Swap();
+    // init blind keys maps
+    const inKeys: Record<string, Buffer> = {};
+    const outKeys: Record<string, Buffer> = {};
+
+    fixture.accept.inputBlindingKeys.forEach((key: string, index: number) => {
+      const script: string = decodedAcceptPsbt.data.inputs[
+        index
+      ].witnessUtxo!.script.toString('hex');
+      inKeys[script] = Buffer.from(key, 'hex');
+    });
+
+    fixture.accept.outputBlindingKeys.forEach((key: string, index: number) => {
+      const script: string = fixture.accept.outputScripts[index];
+      outKeys[script] = Buffer.from(key, 'hex');
+    });
+    const swapRequestSerialized = await swap.request({
+      assetToBeSent: fixture.toBeSent.asset,
+      amountToBeSent: fixture.toBeSent.amount,
+      assetToReceive: fixture.toReceive.asset,
+      amountToReceive: fixture.toReceive.amount,
+      psetBase64: fixture.request.psbt,
+      inputBlindingKeys: inKeys,
+      outputBlindingKeys: outKeys,
+    });
+    const t = await trade.proposeTrade(
+      { baseAsset: '', quoteAsset: '' },
+      TradeType.SELL,
+      swapRequestSerialized
+    );
+    console.log('///trade', t);
+  });
+});
