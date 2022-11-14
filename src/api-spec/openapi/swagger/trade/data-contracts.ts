a02cec36e9f627a73354b18384486204d12f8a19
Janaka-Steph <rochestephane@yahoo.fr>
2022-11-14 16:11:27 +0100

    Update protos and swagger

--- src/api-spec/openapi/swagger/trade/data-contracts.ts
+++ src/api-spec/openapi/swagger/trade/data-contracts.ts
@@ -11,6 +11,7 @@
 
 export interface ProtobufAny {
   '@type'?: string;
+  [key: string]: any;
 }
 
 export interface RpcStatus {
@@ -23,7 +24,6 @@ export interface RpcStatus {
 export interface V1Balance {
   /** @format uint64 */
   baseAmount?: string;
-
   /** @format uint64 */
   quoteAmount?: string;
 }
@@ -52,7 +52,6 @@ export interface V1Fee {
 export interface V1Fixed {
   /** @format int64 */
   baseFee?: string;
-
   /** @format int64 */
   quoteFee?: string;
 }
@@ -82,7 +81,6 @@ export interface V1MarketWithFee {
 export interface V1Preview {
   price?: V1Price;
   fee?: V1Fee;
-
   /** @format uint64 */
   amount?: string;
   asset?: string;
@@ -92,7 +90,6 @@ export interface V1Preview {
 export interface V1PreviewTradeRequest {
   market?: V1Market;
   type?: V1TradeType;
-
   /** @format uint64 */
   amount?: string;
   asset?: string;
@@ -105,7 +102,6 @@ export interface V1PreviewTradeResponse {
 export interface V1Price {
   /** @format double */
   basePrice?: number;
-
   /** @format double */
   quotePrice?: number;
 }
@@ -119,7 +115,6 @@ export interface V1ProposeTradeRequest {
 export interface V1ProposeTradeResponse {
   swapAccept?: V1SwapAccept;
   swapFail?: V1SwapFail;
-
   /** @format uint64 */
   expiryTimeUnix?: string;
 }
@@ -127,38 +122,37 @@ export interface V1ProposeTradeResponse {
 export interface V1SwapAccept {
   /** Random unique identifier for the current message */
   id?: string;
-
   /** indetifier of the SwapRequest message */
   requestId?: string;
-
   /**
    * The partial signed transaction base64 encoded containing the Responder's
    * signed inputs in a PSBT format
    */
   transaction?: string;
-
   /**
    * In case of a confidential transaction the blinding key of each confidential
    * input is included. Each blinding key is identified by the prevout script
    * hex encoded.
    */
   inputBlindingKey?: Record<string, string>;
-
   /**
    * In case of a confidential transaction the blinding key of each confidential
    * output is included. Each blinding key is identified by the output script
    * hex encoded.
    */
   outputBlindingKey?: Record<string, string>;
+  /**
+   * In case of psetv2 transaction, the original list of trader's unblinded inputs,
+   * including also those of the inputs added by the provider.
+   */
+  unblindedInputs?: V1UnblindedInput[];
 }
 
 export interface V1SwapComplete {
   /** Random unique identifier for the current message */
   id?: string;
-
   /** indetifier of the SwapAccept message */
   acceptId?: string;
-
   /**
    * The signed transaction base64 encoded containing the Proposers's signed
    * inputs in a PSBT format
@@ -169,16 +163,13 @@ export interface V1SwapComplete {
 export interface V1SwapFail {
   /** Random unique identifier for the current message */
   id?: string;
-
   /** indetifier of either SwapRequest or SwapAccept message. It can be empty */
   messageId?: string;
-
   /**
    * The failure code. It can be empty
    * @format int64
    */
   failureCode?: number;
-
   /** The failure reason messaged */
   failureMessage?: string;
 }
@@ -186,44 +177,54 @@ export interface V1SwapFail {
 export interface V1SwapRequest {
   /** Random unique identifier for the current message */
   id?: string;
-
   /**
    * The proposer's quantity
    * @format uint64
    */
   amountP?: string;
-
   /** The proposer's asset hash */
   assetP?: string;
-
   /**
    * The responder's quantity
    * @format uint64
    */
   amountR?: string;
-
   /** The responder's asset hash */
   assetR?: string;
-
   /** The proposer's unsigned transaction in PSBT format (base64 string) */
   transaction?: string;
-
   /**
-   * In case of a confidential transaction the blinding key of each confidential
-   * input is included. Each blinding key is identified by the prevout script
-   * hex encoded.
+   * In case of a confidential psetv0 transaction the blinding key of each
+   * confidential input is included. Each blinding key is identified by the
+   * prevout script hex encoded.
    */
   inputBlindingKey?: Record<string, string>;
-
   /**
-   * In case of a confidential transaction the blinding key of each confidential
-   * output is included. Each blinding key is identified by the output script
-   * hex encoded.
+   * In case of a confidential psetv0 transaction the blinding key of each
+   * confidential output is included. Each blinding key is identified by the
+   * output script hex encoded.
    */
   outputBlindingKey?: Record<string, string>;
+  /**
+   * In case of psetv2 transaction, the list of trader's unblinded inputs data,
+   * even in case they are unconfidential.
+   */
+  unblindedInputs?: V1UnblindedInput[];
 }
 
+/** @default "TRADE_TYPE_BUY" */
 export enum V1TradeType {
   TRADE_TYPE_BUY = 'TRADE_TYPE_BUY',
   TRADE_TYPE_SELL = 'TRADE_TYPE_SELL',
+}
+
+/** Custom Types */
+export interface V1UnblindedInput {
+  /** @format int64 */
+  index?: number;
+  asset?: string;
+  /** @format uint64 */
+  amount?: string;
+  assetBlinder?: string;
+  amountBlinder?: string;
 }
